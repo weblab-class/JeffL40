1dd9f23ffa356e278a08ade2bc1f9ead1094baf6
Jeff Li <jli40@stuy.edu>
2020-01-28 21:53:59 -0500

    before deploy

--- server/api.js
+++ server/api.js
@@ -47,11 +47,6 @@ router.post("/initsocket", (req, res) => {
 router.get("/advice", (req, res) => {
   // empty selector means get all documents
   //Advice.deleteMany({}).then((a)=>{1}); //uncomment&refresh to delete all advice
-  // console.log("apiReceivedThisQuery: ", req.query);
-  // console.log("apiReceivedThisQueryID: ", req.query.idQueriedUser);
-  // console.log('req.query.categoryName && req.query.categoryName !== ""'
-  //            , req.query.categoryName && req.query.categoryName !== "");
-  // console.log('req.query.idQueriedUser !== ""', req.query.idQueriedUser !== "");
   if (req.query.categoryName 
         && req.query.categoryName !== ""
         ){
